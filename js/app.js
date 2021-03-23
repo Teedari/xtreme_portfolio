f51d5751d2ef4a7bdb6475907df28503b3289b9c
Dari Godfred <36868807+Teedari@users.noreply.github.com>
2021-03-23 01:41:56 +0000

    first_commit

---
+++ js/app.js
@@ -0,0 +1,31 @@
+(function(header, humberger){
+  let nav_bar_drop = false;
+
+  window.addEventListener('scroll', function(){
+    const header = document.querySelector('header');
+    // console.log(this);
+    header.classList.toggle('sticky', this.scrollY > 0)
+    if(nav_bar_drop){
+      document.querySelector('#navbar').classList.remove('hide')
+      header.classList.remove('drop')
+      humberger.innerHTML = '_';
+    }
+  });
+
+
+  humberger.innerHTML = '_';
+  humberger.addEventListener('click', function(){
+    humberger.innerHTML = '&times;';
+    header.classList.toggle('drop')
+
+    nav_bar_drop = header.classList.value == 'drop' || header.classList.value == 'sticky drop';
+    if(nav_bar_drop){
+      document.querySelector('#navbar').classList.toggle('hide')
+    }else {
+      document.querySelector('#navbar').classList.toggle('hide')
+      humberger.innerHTML = '_';
+    }
+  
+  });
+})(document.querySelector('header'),document.getElementById('humberger'))
+
